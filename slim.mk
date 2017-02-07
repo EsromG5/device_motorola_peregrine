92c64df98271b46ce35d003728908cc32acc55a0
EsromG5 <esromgalaxy5@gmail.com>
2017-02-07 12:08:16 +0000

    tree to slim7

--- slim.mk
+++ slim.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
-PRODUCT_NAME := lineage_peregrine
+PRODUCT_NAME := slim_peregrine
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
