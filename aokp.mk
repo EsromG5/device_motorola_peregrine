35c1c41b344f01ee930764dedd293209bd89e72b
Esrom Cataneo <esromgalaxy5@gmail.com>
2017-04-27 17:00:42 -0300

    Revert "fix to lineage"
    This reverts commit d4d04db9846314ae48e56fd0ffd21267343c7bd7.

--- aokp.mk
+++ aokp.mk
@@ -1,9 +1,12 @@
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
-PRODUCT_NAME := lineage_peregrine
+PRODUCT_NAME := aokp_peregrine
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_NAME="Moto G 4G"
