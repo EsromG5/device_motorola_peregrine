c8f6496614cdb2b66d5d34f74fb606965913ce79
Esrom Cataneo <esromgalaxy5@gmail.com>
2017-06-11 16:40:54 -0300

    TESTES MANIFESTS

--- lineage.mk
+++ lineage.mk
@@ -5,5 +5,5 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
 PRODUCT_NAME := lineage_peregrine
-
+PRODUCT_MODEL := EU ESTOU TESTANDO O MANIFESTS
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
